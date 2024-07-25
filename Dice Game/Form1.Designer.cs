af411173fe74ffbdfb57b2a02003678ea768f242
AGM2004 <ayaangauhar1@gmail.com>
2024-07-25 11:43:24 +0100

    Add project files.

---
+++ Dice Game/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Dice_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox6.Image = global::Dice_Game.Properties.Resources.SIde_1;
+            this.pictureBox6.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(191, 170);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox12.Image = global::Dice_Game.Properties.Resources.SIde_1;
+            this.pictureBox12.Location = new System.Drawing.Point(12, 218);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(191, 170);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(292, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 45);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Uighur", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(12, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Login ?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(219, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Press the button to roll the dice";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 394);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::Dice_Game.Properties.Resources.SIde_1;
+            this.pictureBox1.Location = new System.Drawing.Point(500, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Image = global::Dice_Game.Properties.Resources.SIde_1;
+            this.pictureBox2.Location = new System.Drawing.Point(500, 218);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 170);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(528, 394);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.TabIndex = 21;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(715, 503);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Dice Game/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Dice_Game
+{
+    public partial class Form1 : Form
+    {
+        int i;       
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generator();
+        }
+
+        public void generator()
+        {
+            for (int turn = 0; turn <= 5; turn++)
+            {
+                Random generator = new Random();
+                int random = generator.Next(1, 7);
+                int random2 = generator.Next(1, 7);
+                int random3 = generator.Next(1, 7);
+                int random4 = generator.Next(1, 7);
+                for (i = 0; i < random; i++)
+                {
+                    changeimage(i);
+                }
+
+                for (i = 0; i < random2; i++)
+                {
+                    changeimage2(i);
+                }
+
+                for (i = 0; i < random3; i++)
+                {
+                    changeimage3(i);
+                }
+
+                for (i = 0; i < random4; i++)
+                {
+                    changeimage4(i);
+                }
+
+                int dice1 = random - 1;
+                int dice2 = random2 - 1;
+                int score = 0;                
+                int total = dice1 + dice2;
+                
+                 if (total % 2 == 0)
+                 {
+                    for (int z = 0; z < 100; z++)
+                    {
+                        int sum = score + 10;
+                        textBox1.Text = Convert.ToString(sum);
+                    }                   
+                    
+                }
+            
+
+                 else
+                 {
+                    for (int z = 0; z < 100; z++)
+                    {
+                        int sum = score - 5;
+                        textBox1.Text = Convert.ToString(sum);
+                    }
+                }
+            }
+        }
+             
+
+        private void changeimage(int num)
+        {
+            switch(num) 
+            {
+                case 1:
+                    pictureBox6.Image = Properties.Resources.SIde_1;
+                    break;
+
+                case 2:
+                    pictureBox6.Image = Properties.Resources.Side_2;
+                    break;
+
+                case 3:
+                    pictureBox6.Image = Properties.Resources.Side_3;
+                    break;
+               
+                case 4:
+                    pictureBox6.Image = Properties.Resources.Side_4;
+                    break;
+                
+                case 5:
+                    pictureBox6.Image = Properties.Resources.Side_5;
+                    break;
+                
+                case 6:
+                    pictureBox6.Image = Properties.Resources.Side_6;
+                    break;
+            }
+            
+        }
+
+        private void changeimage3(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.SIde_1;
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Side_2;
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Side_3;
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Side_4;
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Side_5;
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Side_6;
+                    break;
+            }
+
+        }
+
+        private void changeimage4(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.SIde_1;
+                    break;
+
+                case 2:
+                    pictureBox2.Image = Properties.Resources.Side_2;
+                    break;
+
+                case 3:
+                    pictureBox2.Image = Properties.Resources.Side_3;
+                    break;
+
+                case 4:
+                    pictureBox2.Image = Properties.Resources.Side_4;
+                    break;
+
+                case 5:
+                    pictureBox2.Image = Properties.Resources.Side_5;
+                    break;
+
+                case 6:
+                    pictureBox2.Image = Properties.Resources.Side_6;
+                    break;
+            }
+
+        }
+
+        private void changeimage2(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    pictureBox12.Image = Properties.Resources.SIde_1;
+                    break;
+
+                case 2:
+                    pictureBox12.Image = Properties.Resources.Side_2;
+                    break;
+
+                case 3:
+                    pictureBox12.Image = Properties.Resources.Side_3;
+                    break;
+
+                case 4:
+                    pictureBox12.Image = Properties.Resources.Side_4;
+                    break;
+
+                case 5:
+                    pictureBox12.Image = Properties.Resources.Side_5;
+                    break;
+
+                case 6:
+                    pictureBox12.Image = Properties.Resources.Side_6;
+                    break;
+            }
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 login = new Form2();
+            login.Show();
+        }
+       
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            
+        }
+             
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
